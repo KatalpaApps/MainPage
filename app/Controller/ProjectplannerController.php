6c176a9d47dae15abf597b9c2185b43a26700fa5
teodorbusse <teodor.busse@gmail.com>
2014-05-26 08:59:15 +0200

    Ajax Part1

---
+++ app/Controller/ProjectplannerController.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of AboutController
+ *
+ * @author tosiek
+ */
+App::uses('App', 'Controller');
+class ProjectPlannerController extends AppController {
+   
+    public function index(){
+        
+    }
+}
